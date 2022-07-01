290c51f550be020d614b4e7994298675a5621a16
cinyearchan <cinyearchan@gmail.com>
2022-07-01 16:52:06 +0800

    feat: 实现 getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -4,6 +4,8 @@ import { initSlots } from './componentSlots'
 import { emit } from './componentEmit'
 import { PublicInstanceProxyHandlers } from './componentPublicInstance'
 
+let currentInstance = null
+
 export function createComponentInstance(vnode) {
   const component = {
     vnode,
@@ -36,10 +38,16 @@ function setupStatefulComponent(instance: any) {
   const { setup } = Component
 
   if (setup) {
+    // 保存当前实例
+    setCurrentInstance(instance)
+
     // 两个情况，返回 function 或 object
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit
     })
+    
+    // 移除全局变量中保存的当前实例
+    setCurrentInstance(null)
 
     handleSetupResult(instance, setupResult)
   }
@@ -64,4 +72,12 @@ function finishComponentSetup(instance: any) {
   if (Component.render) {
     instance.render = Component.render
   }
+}
+
+export function getCurrentInstance() {
+  return currentInstance
+}
+
+export function setCurrentInstance(instance) {
+  currentInstance = instance
 }
